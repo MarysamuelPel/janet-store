58b0f1e0f48633a7a0dd05e956b9411d611fe417
Mary Samuel <samuelmary1617@gmail.com>
2024-01-18 07:06:06 -0800

    worked on mobile responsiveness

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -20,17 +20,17 @@ const Testimonial = () => {
     {
       img: profilePic2,
       review:
-        "Janet's Store is a hidden gem for beauty services. I recently got a wig installation, and I'm beyond impressed with the quality and professionalism. The staff is friendly, and the ambiance is relaxing. They have a wide range of beauty treatments, making it a one-stop-shop for all your beauty needs.",
+        "I recently got a wig installation. The staff is friendly, and the ambiance is relaxing. They have a wide range of beauty treatments, making it a one-stop-shop for all your beauty needs.",
     },
     {
       img: profilePic3,
       review:
-        "As a guy, I was initially hesitant to try a unisex beauty store, but Janet's exceeded my expectations! The barbing service is top-notch, and they have a great selection of grooming products. The atmosphere is welcoming, and the staff is skilled. I've found my new spot for haircuts and grooming.",
+        "As a guy, I was initially hesitant to try a unisex beauty store, but Janet's exceeded my expectations! The barbing service is top-notch, and they have a great selection of grooming products.",
     },
     {
       img: profilePic4,
       review:
-        "Janet's Beauty Store offers a fantastic range of services. I recently tried their hair coloring, and the results were amazing!The store is clean, and they follow strict hygiene standards. I'm looking forward to trying more services in the future.",
+        "Janet's Beauty Store offers a fantastic range of services. I recently tried their hair coloring, and the results were amazing!The store is clean, and they follow strict hygiene standards.",
     },
   ];
 
